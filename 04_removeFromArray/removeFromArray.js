029086599c92cbd944f49d61ccb939a892442603
Renold Thomas <renoldthomasp@gmail.com>
2024-09-28 11:39:43 -0400

    Edit 04 js file to complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayOfNums, ...removeNums) {
+    let newArray = [];
+    let copy = arrayOfNums;
+    let removedNums = [];
+    for (let i = 0; i < arrayOfNums.length; i++) {
+        for (let j = 0; j < removeNums.length; j++) {
+            if (arrayOfNums[i] === removeNums[j]) {
+                removedNums.push(arrayOfNums[i]);
+                copy[i] = null;
+            }
+        }
+    }
+
+    for (let i = 0; i < copy.length; i++) {
+        if (copy[i] !== null) {
+            newArray.push(copy[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
