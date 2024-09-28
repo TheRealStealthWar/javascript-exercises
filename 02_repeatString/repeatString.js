25138ddf5e00fc41fae1aa66efb3974b1ba57d7b
Renold Thomas <renoldthomasp@gmail.com>
2024-09-28 11:00:10 -0400

    Edit 02 js file to complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (phrase, numOfTimes) {
+    let finalphrase = "";
+    if (numOfTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < numOfTimes; i++) {
+            finalphrase += phrase;
+        }
+        return finalphrase;
+    }
 };
 
 // Do not edit below this line
