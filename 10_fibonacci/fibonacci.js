3e49579d7980570d896c313908bce7dd904b5aed
Renold Thomas <renoldthomasp@gmail.com>
2024-10-01 14:05:30 -0400

    Edit js files to complete 10 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position == 0) {
+        return 0;
+    }
+
+    if (position < 0) {
+        return "OOPS";
+    }
+
+    if (position == 1) {
+        return 1;
+    }
+
+    let fib = 0;
+    let zero = 0;
+    let first = 1;
+
+    for (let i = 1; i < position; i++) {
+        fib = zero + first;
+        zero = first;
+        first = fib;
+    }
+
+    return fib;
 
 };
 
+// console.log(fibonacci("2"));
 // Do not edit below this line
 module.exports = fibonacci;
