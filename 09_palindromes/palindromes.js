afe8dc0dd6f5589c59784ca49944c70dd27e3808
Renold Thomas <renoldthomasp@gmail.com>
2024-10-01 11:10:47 -0400

    Edit js files to complete 09 exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    removedspaces = phrase.split(" ").join("");
+    noPunctuationPhrase = "";
+    for (let i = 0; i < removedspaces.length; i++) {
+        let character = removedspaces.charAt(i);
+        if (!checkPunctuation(character)) {
+            noPunctuationPhrase += character;
+        }
+    }
 
+    noPunctuationPhrase = noPunctuationPhrase.trim().toLowerCase();
+    reversedPhrase = "";
+    for (let i = noPunctuationPhrase.length-1; i >= 0; i--) {
+        reversedPhrase += noPunctuationPhrase.charAt(i);
+    }
+
+    return (noPunctuationPhrase === reversedPhrase);
 };
+
+function checkPunctuation(character) {
+    const punctuations = "~!@#$%^&*()_+\`-={}|[]:;\'<>?,./\""
+    return punctuations.includes(character);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
