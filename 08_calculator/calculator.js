4652e0ee467f5ea94075ee3d0b408c0f3e03776a
Renold Thomas <renoldthomasp@gmail.com>
2024-10-01 10:54:43 -0400

    Edit js files to complete 08 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach((num) => {
+    sum += num;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  array.forEach((num) => {
+    product *= num;
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  while (num > 0) {
+    product *= num;
+    num -= 1;
+  }
+  return product
 };
 
 // Do not edit below this line
